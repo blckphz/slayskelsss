8677db8ebaa35a5af1e3f11755b97865c2c5ff14
blckphz <131730464+blckphz@users.noreply.github.com>
2026-04-02 00:16:59 +0200

    update

--- slayskelsremix/Assets/scripts/DestroyOnAnimationEnd.cs
+++ slayskelsremix/Assets/scripts/DestroyOnAnimationEnd.cs
@@ -1,8 +1,35 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Renderer))]
 public class DestroyOnAnimationEnd : MonoBehaviour
 {
-    // This method will be called by the Animation Event
+    [Header("Wave Settings")]
+    [SerializeField] private float waveSpeed = 2f;
+    [SerializeField] private string shaderProperty = "_WaveDistance";
+
+    private Renderer rend;
+    private MaterialPropertyBlock mpb;
+
+    private float waveDistance = 0f;
+
+    void Awake()
+    {
+        rend = GetComponent<Renderer>();
+        mpb = new MaterialPropertyBlock();
+    }
+
+    void Update()
+    {
+        // Increase wave distance over time
+        waveDistance += Time.deltaTime * waveSpeed;
+
+        // Apply to shader
+        rend.GetPropertyBlock(mpb);
+        mpb.SetFloat(shaderProperty, waveDistance);
+        rend.SetPropertyBlock(mpb);
+    }
+
+    // Called via Animation Event at the end of the explosion
     public void OnAnimationComplete()
     {
         Destroy(gameObject);
