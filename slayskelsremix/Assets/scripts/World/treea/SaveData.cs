9512def188123f1a7526a8e05078d45840dc4c1b
blckphz <danilordlp@gmail.com>
2026-06-03 08:38:13 +0200

    safe

--- slayskelsremix/Assets/scripts/World/treea/SaveData.cs
+++ slayskelsremix/Assets/scripts/World/treea/SaveData.cs
@@ -1,14 +1,13 @@
 using System.Collections.Generic;
 using UnityEngine;
-using static BuildingSaveManager;
 
 [System.Serializable]
 public class SaveData
 {
-    public List<BuildingData> buildings = new();
+    public Vector3 playerPosition; // Added this
+    public List<BuildingSaveManager.BuildingData> buildings = new();
     public List<NpcInventorySaveData> npcInventories = new();
     public List<BushSaveData> bushes = new();
     public List<TreeSaveData> trees = new();
-
     public List<SoilData> soilTiles = new();
 }
