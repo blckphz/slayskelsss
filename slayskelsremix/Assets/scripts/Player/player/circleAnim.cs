aaf685ce5981b454b8d8a4953dfa9d5a1dbdd25b
blckphz <danilordlp@gmail.com>
2026-06-18 05:16:51 +0200

    Revert "Reapply "lil""
    This reverts commit b66041e426a52b11348afa05d220cfe6e413c789.

--- slayskelsremix/Assets/scripts/Player/player/circleAnim.cs
+++ slayskelsremix/Assets/scripts/Player/player/circleAnim.cs
@@ -118,6 +118,7 @@ public class CircleAnim : MonoBehaviour
 
             bool hasTile = tiltManager.Tilemap.GetTile(cell) != null;
 
+            Debug.Log($"[CircleAnim] Checking cell {cell} → Tile: {(hasTile ? "YES" : "NO")}");
 
             if (hasTile)
                 clipToPlay = soilStepClip;
